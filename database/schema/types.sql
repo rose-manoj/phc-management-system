33e1b522447a8e67a7ab35a59e3c1d2f5568af41
rose-manoj <roseemaria08@gmail.com>
2026-07-15 01:07:24 +0530

    feat: modify database schema

---
+++ database/schema/types.sql
@@ -0,0 +1,12 @@
+create type gender_type as enum (
+    'Male',
+    'Female',
+    'Others'
+);
+
+create type job_role as enum(
+    'Medical Officer',
+    'Nurse',
+    'Lab Tech',
+    'Pharmacist'
+);
