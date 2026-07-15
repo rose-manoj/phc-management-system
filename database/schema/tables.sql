0057487addaa3dea3bf05b048bed5e30400ef62f
rose-manoj <roseemaria08@gmail.com>
2026-07-15 12:46:00 +0530

    feat: add staff table

--- database/schema/tables.sql
+++ database/schema/tables.sql
@@ -6,3 +6,12 @@ create table patient(
     contact varchar(15),
     health_status varchar(50)
 );
+
+CREATE TABLE staff(
+    staff_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    contact VARCHAR(15),
+    dob DATE NOT NULL,
+    doj DATE NOT NULL,
+    role job_role NOT NULL
+);
