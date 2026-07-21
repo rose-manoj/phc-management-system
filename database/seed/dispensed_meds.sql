22db99731872955c4a528b591c40a70220abd297
rose-manoj <roseemaria08@gmail.com>
2026-07-22 02:59:31 +0530

    feat: add dispensed medicine records

---
+++ database/seed/dispensed_meds.sql
@@ -0,0 +1,101 @@
+INSERT INTO dispensed_medicines
+(prescription_id, medicine_name, dosage, quantity)
+VALUES
+
+-- Prescription 1
+(1, 'Paracetamol 500 mg', '1 tablet TID for 3 days', 9),
+(1, 'Cetirizine 10 mg', '1 tablet HS for 5 days', 5),
+
+-- Prescription 2
+(2, 'Amlodipine 5 mg', '1 tablet OD', 30),
+
+-- Prescription 3
+(3, 'Metformin 500 mg', '1 tablet BD', 30),
+
+-- Prescription 4
+(4, 'Paracetamol 500 mg', '1 tablet TID', 9),
+(4, 'ORS Sachet', 'After each loose stool', 5),
+
+-- Prescription 5
+(5, 'Ibuprofen 400 mg', '1 tablet BD', 10),
+
+-- Prescription 6
+(6, 'Cetirizine 10 mg', '1 tablet HS', 5),
+(6, 'Paracetamol 500 mg', '1 tablet TID', 9),
+
+-- Prescription 7
+(7, 'Iron Folic Acid', '1 tablet OD', 30),
+
+-- Prescription 8
+(8, 'Chloroquine', 'As directed', 6),
+(8, 'Paracetamol 500 mg', '1 tablet TID', 9),
+
+-- Prescription 9
+(9, 'Ciprofloxacin 500 mg', '1 tablet BD', 10),
+
+-- Prescription 10
+(10, 'Pantoprazole 40 mg', '1 tablet OD', 15),
+
+-- Prescription 11
+(11, 'Paracetamol 500 mg', '1 tablet TID', 9),
+
+-- Prescription 12
+(12, 'Multivitamin', '1 tablet OD', 30),
+
+-- Prescription 13
+(13, 'Amlodipine 5 mg', '1 tablet OD', 30),
+
+-- Prescription 14
+(14, 'Paracetamol 500 mg', '1 tablet TID', 9),
+
+-- Prescription 15
+(15, 'Cetirizine 10 mg', '1 tablet HS', 5),
+
+-- Prescription 16
+(16, 'Iron Folic Acid', '1 tablet OD', 30),
+
+-- Prescription 17
+(17, 'Amoxicillin 500 mg', '1 capsule TID', 15),
+
+-- Prescription 18
+(18, 'Cetirizine 10 mg', '1 tablet HS', 5),
+
+-- Prescription 19
+(19, 'Azithromycin 500 mg', '1 tablet OD', 3),
+(19, 'Paracetamol 500 mg', '1 tablet TID', 9),
+
+-- Prescription 20
+(20, 'Metformin 500 mg', '1 tablet BD', 30),
+
+-- Prescription 21
+(21, 'Povidone Iodine Ointment', 'Apply twice daily', 1),
+
+-- Prescription 22
+(22, 'Iron Folic Acid', '1 tablet OD', 30),
+
+-- Prescription 23
+(23, 'Diclofenac 50 mg', '1 tablet BD', 10),
+
+-- Prescription 24
+(24, 'Paracetamol 500 mg', '1 tablet TID', 9),
+(24, 'Cetirizine 10 mg', '1 tablet HS', 5),
+
+-- Prescription 25
+(25, 'Amoxicillin 500 mg', '1 capsule TID', 15),
+
+-- Prescription 26
+(26, 'Folic Acid', '1 tablet OD', 30),
+
+-- Prescription 27
+(27, 'Amlodipine 5 mg', '1 tablet OD', 30),
+
+-- Prescription 28
+(28, 'Paracetamol 500 mg', '1 tablet TID', 9),
+
+-- Prescription 29
+(29, 'Chloroquine', 'As directed', 6),
+(29, 'Paracetamol 500 mg', '1 tablet TID', 9),
+(29, 'ORS Sachet', 'After each loose stool', 5),
+
+-- Prescription 30
+(30, 'Multivitamin', '1 tablet OD', 30);
